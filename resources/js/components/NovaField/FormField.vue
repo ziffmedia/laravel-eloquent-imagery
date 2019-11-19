0268f4e875d18d07971d0c9931bc878fd93da806
Ralph Schindler <ralph@ralphschindler.com>
2019-11-19 16:20:35 -0600

    style updates

--- resources/js/components/NovaField/FormField.vue
+++ resources/js/components/NovaField/FormField.vue
@@ -3,26 +3,28 @@
     <template slot="field">
       <div class="bg-white rounded-lg">
         <draggable v-model="images" group="image-group" @start="drag=true" @end="drag=false" class="flex flex-wrap mb-2">
-          <div v-for="image in images" class="flex-none pl-1 pr-1">
+          <div v-for="image in images" class="flex-no-grow pl-1 pr-1">
               <image-card-input v-bind:image.sync="image" v-on:remove-image="removeImage"></image-card-input>
           </div>
-        </draggable>
-        <div v-if="(isCollection == false && images.length == 0) || isCollection">
-          <input
-            ref="addNewImageFileInput"
-            class="form-file-input select-none"
-            type="file"
-            :id="`eloquent-imagery-` + this.field.name + `-add-image`"
-            name="name"
-            @change="addImage"
-          />
+          <div slot="footer" class="flex-no-grow pl-1 pr-1">
+            <div v-if="(isCollection == false && images.length == 0) || isCollection" class="px-6 py-4 border border-70 h-full content-center">
+              <input
+                ref="addNewImageFileInput"
+                class="form-file-input select-none"
+                type="file"
+                :id="`eloquent-imagery-` + this.field.name + `-add-image`"
+                name="name"
+                @change="addImage"
+              />
 
-          <span v-on:click="() => this.$refs['addNewImageFileInput'].click()">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="24" width="24">
-              <path d="M6 2h9a1 1 0 0 1 .7.3l4 4a1 1 0 0 1 .3.7v13a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2zm9 2.41V7h2.59L15 4.41zM18 9h-3a2 2 0 0 1-2-2V4H6v16h12V9zm-5 4h2a1 1 0 0 1 0 2h-2v2a1 1 0 0 1-2 0v-2H9a1 1 0 0 1 0-2h2v-2a1 1 0 0 1 2 0v2z"/>
-            </svg>
-          </span>
-        </div>
+              <span v-on:click="() => this.$refs['addNewImageFileInput'].click()">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="72" width="72">
+                  <path fill="#888" d="M6 2h9a1 1 0 0 1 .7.3l4 4a1 1 0 0 1 .3.7v13a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2zm9 2.41V7h2.59L15 4.41zM18 9h-3a2 2 0 0 1-2-2V4H6v16h12V9zm-5 4h2a1 1 0 0 1 0 2h-2v2a1 1 0 0 1-2 0v-2H9a1 1 0 0 1 0-2h2v-2a1 1 0 0 1 2 0v2z"/>
+                </svg>
+            </span>
+            </div>
+          </div>
+        </draggable>
       </div>
 
     </template>
