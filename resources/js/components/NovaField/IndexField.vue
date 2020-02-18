466f0f1ed4e5722560be86187ba3caabefec901d
Seton Brown <seton_brown@ziffdavis.com>
2020-02-18 18:31:58 -0500

    Updating Imagery for index view

--- resources/js/components/NovaField/IndexField.vue
+++ resources/js/components/NovaField/IndexField.vue
@@ -1,11 +1,35 @@
 <template>
-  <span>
-    [ image ]
-  </span>
+    <img :src="images[0]['thumbnailUrl']" class="w-16">
 </template>
 
 <script>
+  import ImageCardDisplay from "./ImageCardDisplay";
+
   export default {
-    props: ['resourceName', 'field'],
+    props: ['resource', 'resourceName', 'resourceId', 'field'],
+
+    components: {
+      ImageCardDisplay,
+    },
+
+    data: () => ({
+      isCollection: false,
+      images: []
+    }),
+
+    mounted () {
+      let images = (this.field.isCollection) ? this.field.value.images : (this.field.value ? [this.field.value] : [])
+
+      this.images = images.map((image, i) => {
+
+        return {
+          inputId: 'eloquent-imagery-' + this.field.name + '-' + i,
+          previewUrl: image.previewUrl,
+          thumbnailUrl: image.thumbnailUrl,
+          metadata: Object.keys(image.metadata).map(key => ({'key': key, 'value': image.metadata[key]}))
+        }
+      })
+
+    }
   }
 </script>
