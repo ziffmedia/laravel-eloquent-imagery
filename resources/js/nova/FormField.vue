fff589bea382d2f8caed10ea51047abe52f9343b
Herton <hertonwork@icloud.com>
2020-10-26 18:17:40 -0400

    When the input is read only hide the options to upload or edit image

--- resources/js/nova/FormField.vue
+++ resources/js/nova/FormField.vue
@@ -4,7 +4,7 @@
       <div class="bg-white rounded-lg">
         <draggable v-model="images" group="image-group" v-on:start="drag=true" v-on:end="drag=false" :class="`flex flex-wrap mb-2 laravel-eloquent-imagery-${this.resourceName}`">
           <div v-for="(image, index) in images" :class="`pl-1 pr-1 border border-70 flex items-end m-1 laravel-eloquent-imagery-image-${(index + 1)}`">
-              <image-card-input v-bind:image.sync="image" v-on:remove-image="removeImage"></image-card-input>
+              <image-card-input :isReadonly="isReadonly" v-bind:image.sync="image" v-on:remove-image="removeImage"></image-card-input>
           </div>
 
           <!--<button v-on:click.prevent="debugThis">De</button>-->
@@ -20,7 +20,7 @@
                 v-on:change="addImage"
               />
 
-              <span v-on:click="() => this.$refs['addNewImageFileInput'].click()">
+              <span v-show="!isReadonly" class="cursor-pointer" v-on:click="() => this.$refs['addNewImageFileInput'].click()">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="72" width="72">
                   <path fill="#888" d="M6 2h9a1 1 0 0 1 .7.3l4 4a1 1 0 0 1 .3.7v13a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2zm9 2.41V7h2.59L15 4.41zM18 9h-3a2 2 0 0 1-2-2V4H6v16h12V9zm-5 4h2a1 1 0 0 1 0 2h-2v2a1 1 0 0 1-2 0v-2H9a1 1 0 0 1 0-2h2v-2a1 1 0 0 1 2 0v2z"/>
                 </svg>
