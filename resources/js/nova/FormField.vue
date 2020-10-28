98ec86cc55fe042dfe3a387c878ac5b318a4e980
Herton <hertonwork@icloud.com>
2020-10-28 17:43:07 -0400

    Hiding button to add more images when is a collection

--- resources/js/nova/FormField.vue
+++ resources/js/nova/FormField.vue
@@ -9,7 +9,7 @@
 
           <!--<button v-on:click.prevent="debugThis">De</button>-->
 
-          <div v-if="(isCollection == false && images.length == 0) || isCollection" slot="footer" class="flex items-center pl-1 pr-1 m-1 border border-70">
+          <div v-show="!isReadonly"  v-if="(isCollection == false && images.length == 0) || isCollection" slot="footer" class="flex items-center pl-1 pr-1 m-1 border border-70">
             <div class="content-center px-6 py-4">
               <input
                 ref="addNewImageFileInput"
@@ -20,7 +20,7 @@
                 v-on:change="addImage"
               />
 
-              <span v-show="!isReadonly" class="cursor-pointer" v-on:click="() => this.$refs['addNewImageFileInput'].click()">
+              <span class="cursor-pointer" v-on:click="() => this.$refs['addNewImageFileInput'].click()">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="72" width="72">
                   <path fill="#888" d="M6 2h9a1 1 0 0 1 .7.3l4 4a1 1 0 0 1 .3.7v13a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2zm9 2.41V7h2.59L15 4.41zM18 9h-3a2 2 0 0 1-2-2V4H6v16h12V9zm-5 4h2a1 1 0 0 1 0 2h-2v2a1 1 0 0 1-2 0v-2H9a1 1 0 0 1 0-2h2v-2a1 1 0 0 1 2 0v2z"/>
                 </svg>
