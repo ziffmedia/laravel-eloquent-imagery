ad2031e54afa959cd8d25e70fe2e4f59646e4c58
ivanmarchenko83 <ivan.marchenko@ziffmedia.com>
2021-03-24 16:29:05 -0400

    crop fix attempt 10

--- src/ImageTransformer/Transformations/Crop.php
+++ src/ImageTransformer/Transformations/Crop.php
@@ -19,50 +19,83 @@ class Crop implements ImagickTransformationInterface
         }
 
         $crop = $arguments->get('crop');
-        $width = (int)$arguments->get('width', 0);
-        $height = (int)$arguments->get('height', 0);
+        $targetWidth = (int)$arguments->get('width', 0);
+        $targetHeight = (int)$arguments->get('height', 0);
 
         [$imgWidth, $imgHeight] = [$imagick->getImageWidth(), $imagick->getImageHeight()];
+
+        //cropping only one side to get target proportion
+        $this->oneSideCrop($imagick, $crop, $imgWidth, $imgHeight, $targetWidth, $targetHeight);
+
+        //now resize to achieve target size
+        foreach ($imagick as $image) {
+            $image->resizeImage(
+                $targetWidth !== 0 ? $targetWidth : $imgWidth,
+                $targetHeight !== 0 ? $targetHeight : $imgHeight,
+                Imagick::FILTER_POINT,
+                1
+            );
+        }
+    }
+
+    private function oneSideCrop(Imagick $imagick, $crop, $imgWidth, $imgHeight, $targetWidth, $targetHeight)
+    {
+        $originProportion = $imgWidth / $imgHeight;
+        $targetProportion = $targetWidth / $targetHeight;
+
+        $newImgWidth = $imgWidth;
+        $newImgHeight = $imgHeight;
 
         $x = 0;
         $y = 0;
 
-        switch ($crop) {
-            case "topCenter":
-                if ($imgWidth > $width)
-                    $x = (int)(($imgWidth - $width) / 2);
-                break;
-            case "center":
-                //means x-center y-center
-                if ($imgWidth > $width)
-                    $x = (int)(($imgWidth - $width) / 2);
-                if ($imgHeight > $height)
-                    $y = (int)(($imgHeight - $height) / 2);
-                break;
-            case "centerRight":
-                //means x-center y-center
-                if ($imgWidth > $width)
-                    $x =$imgWidth - $width;
-                if ($imgHeight > $height)
-                    $y = (int)(($imgHeight - $height) / 2);
-                break;
-            case "centerLeft":
-                //means x-center y-center
-                if ($imgHeight > $height)
-                    $y = (int)(($imgHeight - $height) / 2);
-                break;
-            case "bottomCenter":
-                if ($imgWidth > $width)
-                    $x = (int)(($imgWidth - $width) / 2);
-                if ($imgHeight > $height)
-                    $y = $imgHeight - $height;
-                break;
-            default:
-                return;
+        if ($originProportion < $targetProportion) {
+            //means we need reduce height
+            $newImgHeight = (int)($imgWidth * $targetHeight / $targetWidth);
+            //in this case we do not change width, thus x always 0
+            switch ($crop) {
+                case "topCenter":
+                    $y = 0;
+                    break;
+                case "center":
+                case "centerRight":
+                case "centerLeft":
+                    if ($imgHeight > $newImgHeight)
+                        $y = (int)(($imgHeight - $newImgHeight) / 2);
+                    break;
+                case "bottomCenter":
+                    if ($imgHeight > $newImgHeight)
+                        $y = $imgHeight - $newImgHeight;
+                    break;
+                default:
+                    return;
+            }
+
+        } else {
+            //need reduce width
+            $newImgWidth = (int)($imgHeight * $targetWidth / $targetHeight);
+            //in this case we do not change height, thus y always 0
+            switch ($crop) {
+                case "topCenter":
+                case "bottomCenter":
+                case "center":
+                    if ($imgWidth > $newImgWidth)
+                        $x = (int)(($imgWidth - $newImgWidth) / 2);
+                    break;
+                case "centerRight":
+                    if ($imgWidth > $newImgWidth)
+                        $x = $imgWidth - $newImgWidth;
+                    break;
+                case "centerLeft":
+                    $x = 0;
+                    break;
+                default:
+                    return;
+            }
         }
 
         foreach ($imagick as $image) {
-            $image->cropImage($width, $height, $x, $y);
+            $image->cropImage($newImgWidth, $newImgHeight, $x, $y);
         }
     }
 }
