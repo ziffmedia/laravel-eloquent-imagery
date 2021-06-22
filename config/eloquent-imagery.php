dd640ba2ba62db049bdab86eb9e2aad2b9ae24d7
ivanmarchenko83 <ivan.marchenko@ziffmedia.com>
2021-06-22 12:00:20 -0400

    added skip gifs and config logic

--- config/eloquent-imagery.php
+++ config/eloquent-imagery.php
@@ -126,7 +126,9 @@ return [
                 'fallbackbanner',
                 'crop',
                 'fill'
-            ]
+            ],
+
+            'skip_gifs' => false,
         ]
     ],
 
