526614c47cb409bdd74d750018d4cde58c301aa1
ivanmarchenko83 <ivan.marchenko@ziffmedia.com>
2021-09-21 14:50:20 -0400

    alt text meta test

--- resources/js/nova/ImageCardInput.vue
+++ resources/js/nova/ImageCardInput.vue
@@ -46,14 +46,9 @@
 
                 <h3>Image Metadata</h3>
 
-                <div class="flex px-3" v-for="(metadata, index) in image.metadata">
-                  <input type="text" class="w-1/3 text-xs form-control form-input form-input-bordered m-1" v-model="image.metadata[index].key" />
-                  <input type="text" class="w-full text-xs form-control form-input form-input-bordered m-1" v-model="image.metadata[index].value" />
-                  <span class="cursor-pointer m-2" v-on:click.prevent="removeMetadata(image, index)">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                      <path class="heroicon-ui" d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v14h14V5H5zm11 7a1 1 0 0 1-1 1H9a1 1 0 0 1 0-2h6a1 1 0 0 1 1 1z"/>
-                    </svg>
-                  </span>
+                <div>
+                  <span>Alt Text:</span>
+                  <input type="text" class="w-full text-xs form-control form-input form-input-bordered m-1" v-model="image.metadata['description']" />
                 </div>
 
                 <div class="float-right">
