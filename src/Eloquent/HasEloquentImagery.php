45b5ef0cffab65644514de7f79ae53c3e712c4cd
Ralph Schindler <ralph@ralphschindler.com>
2023-03-14 13:54:31 -0500

    cleanup

--- src/Eloquent/HasEloquentImagery.php
+++ src/Eloquent/HasEloquentImagery.php
@@ -6,11 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 use ReflectionException;
 use RuntimeException;
 
-/**
- * @mixin Model
- *
- * @property array $eloquentImagery
- */
 trait HasEloquentImagery
 {
     public static function bootHasEloquentImagery(): void
@@ -26,50 +21,5 @@ trait HasEloquentImagery
         static::registerModelEvent('saved', [$observer, 'saved']);
         static::registerModelEvent('deleted', [$observer, 'deleted']);
         static::registerModelEvent('replicating', [$observer, 'replicating']);
-    }
-
-    public function initializeHasEloquentImagery(): void
-    {
-        // if (! empty($this->eloquentImageryImages)) {
-        //     throw new RuntimeException('$eloquentImageryImages should be empty, are you sure you have your configuration in the right place?');
-        // }
-        //
-        // if (empty($this->eloquentImagery) || ! property_exists($this, 'eloquentImagery')) {
-        //     throw new RuntimeException('You are using ' . __TRAIT__ . ' but have not yet configured it through $eloquentImagery, please see the docs');
-        // }
-        //
-        // foreach ($this->eloquentImagery as $attribute => $config) {
-        //     if (! isset(static::$eloquentImageryRuntime['configs'][$attribute])) {
-        //         if (is_string($config)) {
-        //             $config = ['path' => $config];
-        //         }
-        //
-        //         if (! is_array($config)) {
-        //             throw new RuntimeException('configuration must be a string or array');
-        //         }
-        //
-        //         $config = [
-        //             'pathTemplate' => $config['path'],
-        //             'presets'      => $config['presets'] ?? [],
-        //             'collection'   => $config['collection'] ?? false,
-        //             'attributes'   => $config['attributes'] ?? [],
-        //         ];
-        //
-        //         static::$eloquentImageryRuntime['configs'][$attribute] = $config;
-        //
-        //         $prototype = app(Image::class, [
-        //             'pathTemplate' => $config['pathTemplate'],
-        //             'presets'      => $config['presets'],
-        //         ]);
-        //
-        //         if (isset($config['collection']) && $config['collection'] === true) {
-        //             $prototype = app(ImageCollection::class, ['imagePrototype' => $prototype]);
-        //         }
-        //
-        //         static::$eloquentImageryRuntime['prototypes'][$attribute] = $prototype;
-        //     }
-        //
-        //     $this->attributes[$attribute] = $this->eloquentImageryImages[$attribute] = clone static::$eloquentImageryRuntime['prototypes'][$attribute];
-        // }
     }
 }
