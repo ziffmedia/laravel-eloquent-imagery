dc8a21bae199f5124d3a845fccd73d81f0bb9eef
Ralph Schindler <ralph@ralphschindler.com>
2024-02-27 09:25:37 -0600

    demo updates

--- demo/app/Models/ImageCollectionExample.php
+++ demo/app/Models/ImageCollectionExample.php
@@ -3,13 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use ZiffMedia\LaravelEloquentImagery\Eloquent\EloquentImageCast;
 use ZiffMedia\LaravelEloquentImagery\Eloquent\EloquentImageCollectionCast;
-use ZiffMedia\LaravelEloquentImagery\Eloquent\HasEloquentImagery;
 
 class ImageCollectionExample extends Model
 {
-    use HasEloquentImagery;
+    // use HasEloquentImagery;
 
     protected $casts = [
         'images' => EloquentImageCollectionCast::class . ':image-collection-examples/{id}/{index}.{extension}'
--- demo/app/Models/SingleImageExample.php
+++ demo/app/Models/SingleImageExample.php
@@ -4,12 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use ZiffMedia\LaravelEloquentImagery\Eloquent\EloquentImageCast;
-use ZiffMedia\LaravelEloquentImagery\Eloquent\HasEloquentImagery;
 
 class SingleImageExample extends Model
 {
-    use HasEloquentImagery;
-
     protected $casts = [
         'variations' => 'json',
         'image' => EloquentImageCast::class . ':single-image-examples/{id}.{extension}'
