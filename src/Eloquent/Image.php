9f2811781cbf05e623f252bd83ac93b618b4c4cc
sdiazz <samantha.diaz@ziffmedia.com>
2025-12-01 12:09:25 -0500

    add svg to mime type extensions

--- src/Eloquent/Image.php
+++ src/Eloquent/Image.php
@@ -39,6 +39,7 @@ class Image implements JsonSerializable
         'image/gif' => 'gif',
         'image/webp' => 'webp',
         'image/bmp' => 'bmp',
+        'image/svg+xml' => 'svg+xml',
     ];
 
     public ?Filesystem $filesystem = null;
